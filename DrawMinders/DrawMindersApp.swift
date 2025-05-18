2d98a8f1e5387c661e3b2e70e405222837ea24e8
PatrykPNG <ostrowskiszkola8@gmail.com>
2025-05-18 15:33:09 +0200

    Initial Commit

---
+++ DrawMinders/DrawMindersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawMindersApp.swift
+//  DrawMinders
+//
+//  Created by Patryk Ostrowski on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct DrawMindersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
